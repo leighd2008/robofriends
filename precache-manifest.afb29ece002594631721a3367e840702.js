f2cb0fcad24c4d69445b483bac37349bf7c62918
leighd2008 <leighd2008@gmail.com>
2019-01-23 07:17:52 -0500

    Updates

---
+++ precache-manifest.afb29ece002594631721a3367e840702.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "1446d6ec194445c3572d",
+    "url": "/robofriends/static/js/runtime~main.1446d6ec.js"
+  },
+  {
+    "revision": "75f535a1f166db7d2155",
+    "url": "/robofriends/static/js/main.75f535a1.chunk.js"
+  },
+  {
+    "revision": "b2dfcadf5761bda471d2",
+    "url": "/robofriends/static/js/1.b2dfcadf.chunk.js"
+  },
+  {
+    "revision": "75f535a1f166db7d2155",
+    "url": "/robofriends/static/css/main.4a4dad34.chunk.css"
+  },
+  {
+    "revision": "b2dfcadf5761bda471d2",
+    "url": "/robofriends/static/css/1.1de8015d.chunk.css"
+  },
+  {
+    "revision": "ad57c2cee460c8f988a32c2393cf3a92",
+    "url": "/robofriends/index.html"
+  }
+];
