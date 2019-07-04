b4246d0edba6bb42b7bbffee46e2ec08a9a35ac3
leighd2008 <leighd2008@gmail.com>
2019-07-04 08:21:27 -0400

    Updates

---
+++ precache-manifest.0457b7085647725c52ab0d1527d7be3e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93848d86a7c25fe5604143c453676b44",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "4d4662b11c2c669a73d6",
+    "url": "/robofriends/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "bd417ca086f43cec8c76",
+    "url": "/robofriends/static/css/main.7e7653c1.chunk.css"
+  },
+  {
+    "revision": "4d4662b11c2c669a73d6",
+    "url": "/robofriends/static/js/2.2b3ff20d.chunk.js"
+  },
+  {
+    "revision": "bd417ca086f43cec8c76",
+    "url": "/robofriends/static/js/main.66195189.chunk.js"
+  },
+  {
+    "revision": "143eb19aa518302daf32",
+    "url": "/robofriends/static/js/runtime~main.fb41d28d.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/robofriends/precache-manifest.df85ea470aff5bc6e3f75ad6913bba9f.js"
+  "/robofriends/precache-manifest.0457b7085647725c52ab0d1527d7be3e.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/robofriends/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/robofriends/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
